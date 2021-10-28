435c53053569350256dbf3439df1162e11abf08f
Andra <andra.suciu2006@yahoo.com>
2021-10-28 21:38:43 +0300

    updated images links

--- iChef-WebUi/src/app/components/recipes/recipes.component.ts
+++ iChef-WebUi/src/app/components/recipes/recipes.component.ts
@@ -8,9 +8,9 @@ import { Recipe } from 'src/app/data-types/recipe';
 })
 export class RecipesComponent implements OnInit {
 
-  recipe1: Recipe = new Recipe("Pasta Carbonara","Easy", "../../../../../assets/imgs/pasta.jpg",4.5);
-  recipe2: Recipe = new Recipe("Pizza Quattro Fromaggi","Easy", "../../../../../assets/imgs/pizza.jpg",4.0);
-  recipe3: Recipe = new Recipe("Burger","Easy", "../../../../../assets/imgs/burger.jpg",4.8);
+  recipe1: Recipe = new Recipe("Pasta Carbonara","Easy", "/assets/imgs/pasta.jpg",4.5);
+  recipe2: Recipe = new Recipe("Pizza Quattro Fromaggi","Easy", "/assets/imgs/pizza.jpg",4.0);
+  recipe3: Recipe = new Recipe("Burger","Easy", "/assets/imgs/burger.jpg",4.8);
 
   constructor() { }
 
