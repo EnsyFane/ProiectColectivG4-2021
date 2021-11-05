f25907dcc6352f0ff631c17991c9833f8497cfc8
Madalina Zele <zmir2845@scs.ubbcluj.ro>
2021-11-05 02:30:40 +0200

    used Lombok

--- iChef-WebApi/src/main/java/com/kitchen/iChef/Controller/Request/LogInRequest.java
+++ iChef-WebApi/src/main/java/com/kitchen/iChef/Controller/Request/LogInRequest.java
@@ -1,30 +1,23 @@
 package com.kitchen.iChef.Controller.Request;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@Setter
+@ToString
 public class LogInRequest {
+
+    @NotNull(message = "The email of the user is not valid!")
+    @Pattern(regexp = "^[a-zA-Z]+[a-zA-Z0-9.]*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$",
+            message = "The email of the user is not valid!")
     private String email;
+
+    @NotBlank(message = "The password is not valid!")
     private String password;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "LogInRequest{" +
-                "email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
