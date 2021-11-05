43e136851070078b87171920458c0ddb57664263
Madalina Zele <zmir2845@scs.ubbcluj.ro>
2021-11-05 02:28:35 +0200

    used Lombok

--- iChef-WebApi/src/main/java/com/kitchen/iChef/Controller/Request/SignUpRequest.java
+++ iChef-WebApi/src/main/java/com/kitchen/iChef/Controller/Request/SignUpRequest.java
@@ -1,9 +1,16 @@
 package com.kitchen.iChef.Controller.Request;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
+@Getter
+@Setter
+@ToString
 public class SignUpRequest {
 
     @NotBlank(message = "The first name of the user is not valid!")
@@ -22,55 +29,4 @@ public class SignUpRequest {
 
     @NotBlank(message = "The password is not valid!")
     private String password;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "SignUpRequest{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", username='" + username + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
