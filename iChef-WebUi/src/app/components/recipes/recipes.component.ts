b573f5e81bf94ff81565a014ef8e52623b22a308
PinkyPotrick <62207615+PinkyPotrick@users.noreply.github.com>
2021-11-08 17:23:27 +0200

    users/pfilip/start page: fixed an error

--- iChef-WebUi/src/app/components/recipes/recipes.component.ts
+++ iChef-WebUi/src/app/components/recipes/recipes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Recipe } from 'src/app/data-types/recipe';
-import { BUTTON, PLACEHOLDERS } from '../../constants/texts';
+import { BUTTON_STRINGS, PLACEHOLDERS_STRINGS } from '../../constants/texts';
 
 @Component({
   selector: 'app-recipes',
@@ -9,8 +9,8 @@ import { BUTTON, PLACEHOLDERS } from '../../constants/texts';
 })
 export class RecipesComponent {
 
-    readonly searchBtn = BUTTON.SEARCH;
-    readonly searchPlaceHolder = PLACEHOLDERS.SEARCH;
+    readonly searchBtn = BUTTON_STRINGS.SEARCH;
+    readonly searchPlaceHolder = PLACEHOLDERS_STRINGS.SEARCH;
 
     recipe1: Recipe = new Recipe('Pasta Carbonara', 'Easy', '/assets/imgs/pasta.jpg', 4.5);
     recipe2: Recipe = new Recipe('Pizza Quattro Fromaggi', 'Easy', '/assets/imgs/pizza.jpg', 4.0);
