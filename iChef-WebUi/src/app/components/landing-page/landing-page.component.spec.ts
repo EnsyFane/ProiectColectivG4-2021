83647023ed4f10fa04c647b518625c0823f74394
Andra <andra.suciu2006@yahoo.com>
2021-11-11 22:11:55 +0200

    fixed errors 3

--- iChef-WebUi/src/app/components/landing-page/landing-page.component.spec.ts
+++ iChef-WebUi/src/app/components/landing-page/landing-page.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingPageComponent } from './landing-page.component';
@@ -8,7 +9,8 @@ describe('LandingPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LandingPageComponent ]
+      declarations: [ LandingPageComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
   });
--- iChef-WebUi/src/app/components/recipes/recipe-card/recipe-card.component.spec.ts
+++ iChef-WebUi/src/app/components/recipes/recipe-card/recipe-card.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Recipe } from 'src/app/data-types/recipe';
 
@@ -9,7 +10,8 @@ describe('RecipeCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RecipeCardComponent ]
+      declarations: [ RecipeCardComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
   });
