f23e3b7c062df1fa64c3abb12f2dda39eea100e3
Stefan Tataran <fane_ecl@yahoo.com>
2021-12-15 15:18:29 +0200

    fix test

--- iChef-WebUi/src/app/components/upload-image-form/upload-image-form.component.spec.ts
+++ iChef-WebUi/src/app/components/upload-image-form/upload-image-form.component.spec.ts
@@ -1,25 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { UploadImageFormComponent } from './upload-image-form.component';
 
 describe('UploadImageFormComponent', () => {
-  let component: UploadImageFormComponent;
-  let fixture: ComponentFixture<UploadImageFormComponent>;
+    let component: UploadImageFormComponent;
+    let fixture: ComponentFixture<UploadImageFormComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ UploadImageFormComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [UploadImageFormComponent],
+            imports: [
+                FormsModule,
+                ReactiveFormsModule
+            ]
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UploadImageFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(UploadImageFormComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- iChef-WebUi/src/app/services/imgur.service.spec.ts
+++ iChef-WebUi/src/app/services/imgur.service.spec.ts
@@ -1,12 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { FakeHttpClient } from '../testing/fake-http-client';
 
 import { ImgurService } from './imgur.service';
+import { SnackbarService } from './snackbar/snackbar.service';
 
 describe('ImgurService', () => {
     let service: ImgurService;
+    let fakeHttpClient: FakeHttpClient;
+    const fakeSnackbarService = jasmine.createSpyObj(['displaySnackbar', 'displayErrorSnackbar']);
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        fakeHttpClient = new FakeHttpClient();
+        TestBed.configureTestingModule({
+            providers: [
+                { provide: SnackbarService, useValue: fakeSnackbarService },
+                { provide: HttpClient, useValue: fakeHttpClient }
+            ]
+        });
         service = TestBed.inject(ImgurService);
     });
 
